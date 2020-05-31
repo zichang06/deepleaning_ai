4d4326e481cbf6ae5477c7c6ea5d411b5ddb2618
zichang06 <caoruixunzhao2006@126.com>
2020-05-31 11:51:56 +0800

    update

--- assignment5_3/Machine Translation/nmt_utils.py
+++ assignment5_3/Machine Translation/nmt_utils.py
@@ -8,7 +8,7 @@ import keras.backend as K
 import matplotlib.pyplot as plt
 
 fake = Faker()
-fake.seed(12345)
+Faker.seed(12345)
 random.seed(12345)
 
 # Define format of the data we would like to generate
